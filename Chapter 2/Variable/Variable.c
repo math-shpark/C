b22fdc9fec77c96c95b022da4ecb35d7c1da8008
Su-ho Park <psuhoo122065@gmail.com>
2022-07-22 10:44:06 +0900

    FEAT : Sample Code of Variable

---
+++ Chapter 2/Variable/Variable.c
@@ -0,0 +1,16 @@
+// Variable.c
+
+#include <stdio.h>
+
+int main(void) {
+	int num1, num2; // 선언
+	num1 = 5; // 초기화
+	num2 = 15;
+
+	int num3 = 10, num4 = 20; // 선언 및 초기화
+
+	printf("num1 : %d, num2 : %d\n", num1, num2);
+	printf("num3 : %d, num4 : %d", num3, num4);
+
+	return 0;
+}
